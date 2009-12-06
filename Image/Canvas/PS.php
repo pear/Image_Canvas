c0c5c4f90b1dc3dd5ef1fab0a65b32b91f30c192
Stefan Neufeind <neufeind@php.net>
2009-12-06 12:11:32 +0000

    Fix deprecation-warnings with PHP 5.3.x (ereg -> preg_match)
    git-svn-id: https://svn.php.net/repository/pear/packages/Image_Canvas/trunk@291761 c90b9560-bf6c-de11-be94-00142212c4b1

--- Image/Canvas/PS.php
+++ Image/Canvas/PS.php
@@ -991,14 +991,14 @@ class Image_Canvas_PS extends Image_Canvas
             $php_info = ob_get_contents();
             ob_end_clean();
 
-            if (ereg("<td[^>]*>pslib Version *<\/td><td[^>]*>([^<]*)<\/td>",
+            if (preg_match("/<td[^>]*>pslib Version *<\/td><td[^>]*>([^<]*)<\/td>/",
                 $php_info, $result))
             {
                 $version = $result[1];
             }
         }               
         
-        if (ereg('([0-9]{1,2})\.[0-9]{1,2}(\.[0-9]{1,2})?', trim($version), $result)) {
+        if (preg_match('/([0-9]{1,2})\.[0-9]{1,2}(\.[0-9]{1,2})?/', trim($version), $result)) {
             return $result[1];
         } else {
             return $version;
